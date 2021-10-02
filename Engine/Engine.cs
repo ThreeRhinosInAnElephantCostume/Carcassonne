fd4c6c4bc218aad97e1af770f204b7679c5ed81f
developer <you@example.com>
2021-10-02 22:24:12 +0200

    An idea for the state machine, some bulk renames.

--- Engine/Engine.cs
+++ Engine/Engine.cs
@@ -17,5 +17,29 @@ using static Utils;
 
 public partial class Engine
 {
+    public struct Action
+    {
+        uint libraryid;
+        uint actionid;
+        byte[] actiondata;
+    }
+    public abstract class Governor
+    {
+        bool CanExecute(State state, Action move);
+        void Execute(State state, Action move);
+        List<Action> BasicActions();
+        List<Action> PossibleActions();
+    }
+    public abstract class PawnModule
+    {
+        
+    }
+    public class Meeple : PawnModule
+    {
 
+    }
+    static List<Governor> possiblegovernors = new List<Governor>();
+    static List<Governor> possiblepawns = new List<PawnModule>();
+
+    State state = new State();
 }
