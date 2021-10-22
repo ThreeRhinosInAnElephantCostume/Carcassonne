7d592fc4af3d921692abf6943d4bc7e2334263a8
ThreeRhinosInAnElephantCostume <you@example.com>
2021-10-22 12:17:18 +0200

    Mockups

---
+++ Mockups/MLoadGameList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Godot;
+using static Utils;
+
+[Tool]
+public class MLoadGameList : ItemList
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    void LoadItems()
+    {
+        Clear();
+        RepeatN(5, i => AddItem($"<SAVE DATA {i}>"));
+    }
+    public override void _Ready()
+    {
+        LoadItems();
+    }
+    public override void _Process(float delta)
+    {
+        if (GetItemCount() == 0)
+        {
+            LoadItems();
+        }
+    }
+}
