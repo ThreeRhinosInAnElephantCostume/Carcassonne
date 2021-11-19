716c7769c8a080509ca77c30f909f6d8613b912b
ThreeRhinosInAnElephantCostume <you@example.com>
2021-11-20 00:16:59 +0100

    Working on the relay

---
+++ Networking/Connection.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using Carcassonne;
+using ExtraMath;
+using Godot;
+using Newtonsoft.Json;
+using static System.Math;
+using static Carcassonne.GameEngine;
+using static Networking;
+using static Utils;
+
+public static partial class Networking
+{
+    public abstract partial class Peer
+    {
+        public class Connection
+        {
+            public long ID;
+            public IPAddress Address;
+            public ushort Port;
+            public string PublicKey { get; set; }
+            public string PrivateKey { get; set; }
+            public DateTime LastUpdate { get; protected set; }
+            public DateTime FirstUpdate { get; protected set; }
+            public DateTime LastSent { get; protected set; }
+            public IPEndPoint Endpoint => new IPEndPoint(Address, Port);
+            public bool IsPingValid { get; protected set; } = false;
+            public double Ping { get; protected set; } = 0; // in seconds, measured over 10 samples
+            public double PacketLoss { get; protected set; } = 0; // 0.0 - 1.0, in the past 100 packets
+            public bool Disconnected { get; set; } = false;
+
+            FIFO<double> _pingCount = new FIFO<double>(PING_SAMPLES);
+            FIFO<bool> _packetLossCount = new FIFO<bool>(PACKET_LOSS_SAMPLES);
+
+            public ulong TotalSentTo { get; protected set; } = 0;
+            public ulong TotalReceivedFrom { get; protected set; } = 0;
+            public ulong TotalAccepted { get; protected set; } = 0;
+            public ulong TotalDiscared { get; protected set; } = 0;
+            public ulong TotalRejected { get; protected set; } = 0;
+            public ulong TotalMalformed { get; protected set; } = 0;
+            public ulong TotalErrorous { get; protected set; } = 0;
+            public ulong TotalLost { get; protected set; } = 0;
+            public ulong TotalDelayed { get; protected set; } = 0;
+            DateTime _minuteUpdate = DateTime.Now;
+            ulong _messagesThisMinute = 0;
+            DateTime _secondUpdate = DateTime.Now;
+            ulong _messagesThisSecond = 0;
+            public long LastID { get; protected set; }
+            long _oldestAcceptable = 0;
+            long _gapstart = 0;
+            public List<(ReceivedMessage rmsg, DateTime rtime)> DelayedMessages = new List<(ReceivedMessage, DateTime)>();
+            void UpdatePing(double s)
+            {
+                _pingCount.Enqueue(s);
+                IsPingValid = _pingCount.Full;
+                Ping = 0;
+                foreach (var v in _pingCount)
+                {
+                    Ping += v / _pingCount.Count;
+                }
+            }
+            void UpdatePacketLoss()
+            {
+                if (_packetLossCount.Full)
+                {
+                    double pl = 0;
+                    foreach (var it in _packetLossCount)
+                    {
+                        pl += (it) ? 0 : 1;
+                    }
+                    PacketLoss /= _packetLossCount.Capacity;
+                }
+            }
+            void UpdateConnectionStatus()
+            {
+                UpdatePacketLoss();
+                var sincelast = DateTime.Now - LastUpdate;
+                Assert<NetConnectionTimeoutException>(sincelast.TotalMilliseconds < CONNECTION_TIMEOUT_MS);
+                Assert<NetConnectionDDOSException>(_messagesThisMinute < MAX_MESSAGES_PER_MINUTE);
+                Assert<NetConnectionDDOSException>(_messagesThisSecond < MAX_MESSAGES_PER_SECOND);
+                Assert<NetConnectionInsaneException>(TotalErrorous < MAX_ERROROUS);
+            }
+            public void Touch()
+            {
+                LastUpdate = DateTime.Now;
+            }
+            public void NoteDeayed()
+            {
+                Touch();
+                TotalReceivedFrom++;
+                TotalDelayed++;
+                UpdateConnectionStatus();
+            }
+            public void NoteMessage(ReceivedMessage rmsg, MessageReaction reaction, TimeSpan time, bool wasdelayed)
+            {
+                Touch();
+                LastID = rmsg.Msg.MessageID;
+                if (reaction == MessageReaction.DISCARDED)
+                    return;
+                _packetLossCount.Enqueue(true);
+                if (!wasdelayed)
+                    TotalReceivedFrom++;
+                var x = reaction switch
+                {
+                    MessageReaction.ACCEPTED => TotalAccepted++,
+                    MessageReaction.DISCARDED => TotalDiscared++,
+                    MessageReaction.REJECTED => TotalRejected++,
+                    MessageReaction.MALFORMED => TotalMalformed++,
+                    MessageReaction.ERROROUS => TotalErrorous++,
+                    _ => throw new AssertionFailureException("Invalid message reaction"),
+                };
+
+                var now = DateTime.Now;
+
+                if ((now - _secondUpdate).TotalSeconds > 1)
+                    _messagesThisSecond = 0;
+                _messagesThisSecond++;
+
+                if ((now - _secondUpdate).TotalMinutes > 1)
+                    _messagesThisMinute = 0;
+                _messagesThisMinute++;
+
+                UpdatePing(time.TotalSeconds);
+                UpdateConnectionStatus();
+            }
+            public void NoteMessageSent()
+            {
+                TotalSentTo++;
+                UpdateConnectionStatus();
+                LastSent = DateTime.Now;
+            }
+            public void NoteMessageConfirmed()
+            {
+                _packetLossCount.Enqueue(true);
+            }
+            public void NoteMessageLost()
+            {
+                TotalLost++;
+                _packetLossCount.Enqueue(false);
+                UpdateConnectionStatus();
+            }
+            long _idCounter = 1;
+            public long GenerateMessageID()
+            {
+                return _idCounter++;
+            }
+            public Connection(long ID, IPAddress address, ushort port, string publickey, string privatekey)
+            {
+                this.ID = ID;
+                this.Address = address;
+                this.Port = port;
+                this.LastUpdate = DateTime.Now;
+                this.FirstUpdate = DateTime.Now;
+                this.PublicKey = publickey;
+                this.PrivateKey = privatekey;
+            }
+        }
+    }
+}
---
+++ Networking/RelayServer.cs
@@ -0,0 +1,380 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Net;
+using System.Net.Sockets;
+using System.Reflection;
+using System.Reflection.Metadata;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading;
+using Carcassonne;
+using ExtraMath;
+using Godot;
+using Newtonsoft.Json;
+using static System.Math;
+using static Carcassonne.GameEngine;
+using static Networking;
+using static Utils;
+public static partial class Networking
+{
+    public enum RelayMessage
+    {
+        RELAY_BEFORE_FIRST = MessageType.MESAGE_TYPE_AFTER_LAST,
+
+        RELAY_LOGIN_ADMIN,
+        RELAY_LOGIN_CLIENT,
+        RELAY_NOTIFY_LOGIN,
+        RELAY_NOTIFY_KNOWN_HOST,
+        RELAY_NOTIFY_DISCONNECT,
+        RELAY_NOTIFY_SHUTDOWN,
+        RELAY_GET_MY_STATUS,
+
+        RELAY_ADMIN_BEFORE_FIRST,
+        RELAY_ADMIN_KICK_MEMBER,
+        RELAY_ADMIN_BAN_MEMBER,
+        RELAY_ADMIN_SET_PASSWORD,
+        RELAY_ADMIN_POWER_OFF,
+        RELAY_ADMIN_AFTER_LAST,
+        RELAY_CLIENT_BEFORE_FIRST = RELAY_ADMIN_AFTER_LAST,
+        RELAY_CLIENT_GET_STATUS,
+        RELAY_CLIENT_AFTER_LAST,
+        RELAY_AFTER_LAST,
+    }
+    public enum DisconnectType
+    {
+        END = 0,
+        KICK,
+        BAN,
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    struct MLogin
+    {
+        public string password;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    struct MSetPassword
+    {
+        public string password;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    struct MLoginResponse
+    {
+        public bool granted;
+        public long newid;
+        public bool isadmin;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    struct MNotifyDisconnected
+    {
+        public long DisconnectedID;
+        public DisconnectType Reason;
+    }
+
+    [StructLayout(LayoutKind.Sequential)]
+    struct MNotifyLoggedExists
+    {
+        public long id;
+        public bool isnew;
+        public bool isadmin;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    struct MStatus
+    {
+        public bool isadmin;
+        public bool islogged;
+    }
+    [StructLayout(LayoutKind.Sequential)]
+    struct MKickBan
+    {
+        public long ID;
+    }
+
+    class RelayServer : Peer
+    {
+        Action<NetException> _failHandle;
+        Connection _adminConnection;
+        List<Connection> _loggedIn = new List<Connection>();
+        string _adminPassword;
+        string _clientPassword = null;
+        bool _adminLogging = false;
+        public List<Connection> LoggedIn { get { lock (_loggedIn) { return _loggedIn.ToList(); } } }
+        protected bool IsAllowedToMessage(Message msg, Connection con)
+        {
+            bool islogged = _loggedIn.Contains(con);
+            bool isadmin = con == _adminConnection;
+            if (msg.MType > (int)RelayMessage.RELAY_ADMIN_BEFORE_FIRST && msg.MType < (int)RelayMessage.RELAY_ADMIN_AFTER_LAST)
+                return isadmin;
+            if (msg.MType > (int)RelayMessage.RELAY_CLIENT_BEFORE_FIRST && msg.MType < (int)RelayMessage.RELAY_CLIENT_AFTER_LAST)
+                return islogged;
+            return true;
+        }
+        void RelayMessageTo(Message msg, Connection target, long sourceID, bool modsenderreceiver = true)
+        {
+            if (modsenderreceiver)
+            {
+                msg.SenderID = sourceID;
+                msg.ReceiverID = target.ID;
+            }
+            msg.MessageID = target.GenerateMessageID();
+            QueueSendMessage(msg, msg.RequestConfirmation);
+        }
+        void RelayToAll(Connection from, Message msg, bool modsenderreceiver = true, bool includefrom = false)
+        {
+            List<Connection> c; ;
+            lock (_loggedIn)
+            {
+                c = _loggedIn.ToList();
+            }
+            if (!includefrom && c.Contains(from))
+                c.Remove(from);
+            c.ForEach(it =>
+            {
+                RelayMessageTo(msg, it, from.ID, modsenderreceiver);
+            });
+        }
+        void SendToAll(Message msg, bool requestreturn = true)
+        {
+            List<Connection> c; ;
+            lock (_loggedIn)
+            {
+                c = _loggedIn.ToList();
+            }
+            c.ForEach(it => QueueSendMessage(msg, requestreturn));
+        }
+        protected override void DisconnectConnection(Connection con, DisconnectReason reason)
+        {
+            base.DisconnectConnection(con, reason);
+
+            Connections.Remove(con);
+            lock (_loggedIn)
+            {
+                _loggedIn.Remove(con);
+            }
+        }
+        protected MessageReaction ProcessRelayMessage(Message msg, Connection con)
+        {
+            if (msg.MType > (int)RelayMessage.RELAY_BEFORE_FIRST && msg.MType < (int)RelayMessage.RELAY_AFTER_LAST)
+                return MessageReaction.ERROROUS;
+            try
+            {
+                switch ((RelayMessage)msg.MType)
+                {
+                    case RelayMessage.RELAY_LOGIN_CLIENT:
+                    case RelayMessage.RELAY_LOGIN_ADMIN:
+                        {
+                            bool isadmin = (RelayMessage)msg.MType == RelayMessage.RELAY_LOGIN_ADMIN;
+                            if (msg.IsResponse)
+                                return MessageReaction.MALFORMED;
+                            if (_adminLogging && isadmin)
+                                return MessageReaction.REJECTED;
+                            var m = DeserializeStruct<MLogin>(msg.Data);
+
+                            bool granted = (isadmin && m.password == _adminPassword || (!isadmin && m.password == _clientPassword));
+                            if (!granted)
+                            {
+                                QueueSendMessage(BasicMessage(con, (int)RelayMessage.RELAY_LOGIN_ADMIN, true, new MLoginResponse()
+                                {
+                                    granted = granted,
+                                }), true);
+                            }
+                            else
+                            {
+                                if (isadmin)
+                                {
+                                    AlternativeAddresses.Add(con.ID, ID_ADMIN);
+                                }
+                                _adminLogging = true;
+                                QueueSendMessage(BasicMessage(con, (int)RelayMessage.RELAY_LOGIN_ADMIN, true, new MLoginResponse()
+                                {
+                                    granted = (m.password == _adminPassword),
+                                    newid = (isadmin) ? ID_ADMIN : con.ID,
+                                    isadmin = isadmin,
+                                }), (sm) =>  // fail
+                                {
+                                    _adminLogging = false;
+                                    AlternativeAddresses.Remove(con.ID);
+                                }, (sm) =>  // success
+                                {
+                                    if (isadmin)
+                                    {
+                                        _adminConnection = con;
+                                        con.ID = ID_ADMIN;
+                                    }
+                                    lock (_loggedIn)
+                                    {
+                                        _loggedIn.Add(con);
+                                    }
+                                    LogEvent($"Successful login as {((isadmin) ? "ADMIN" : "CLIENT")}. ID: {con.ID}.", Severity.INFO);
+                                });
+                            }
+                            break;
+                        }
+                    case RelayMessage.RELAY_NOTIFY_SHUTDOWN:
+                    case RelayMessage.RELAY_NOTIFY_KNOWN_HOST:
+                        {
+                            return MessageReaction.ERROROUS;
+                        }
+                    case RelayMessage.RELAY_NOTIFY_DISCONNECT:
+                        {
+                            if (_loggedIn.Contains(con))
+                            {
+                                RelayToAll(con, msg);
+                                DisconnectConnection(con, DisconnectReason.END);
+                            }
+                            break;
+                        }
+                    case RelayMessage.RELAY_ADMIN_BAN_MEMBER:
+                    case RelayMessage.RELAY_ADMIN_KICK_MEMBER:
+                        {
+                            bool ban = msg.MType == (int)RelayMessage.RELAY_ADMIN_BAN_MEMBER;
+                            var m = DeserializeStruct<MKickBan>(msg.Data);
+                            if (m.ID == ID_ALL || m.ID == ID_RELAY || m.ID == ID_ADMIN)
+                            {
+                                LogEvent($"Attempting to kick/ban a protected ID. FROM: {msg.SenderID} REMID: {m.ID}", Severity.ERROR);
+                                return MessageReaction.ERROROUS;
+                            }
+                            var cons = LoggedIn;
+                            var found = cons.Find(it => MatchIDs(it.ID, m.ID));
+                            if (found == null)
+                            {
+                                LogEvent($"Attempted to kick/ban a non-existent peer. FROM: {msg.SenderID} REM: {m.ID}", Severity.WARNING);
+                                return MessageReaction.MALFORMED;
+                            }
+                            SendToAll(BasicMessage(found, (int)RelayMessage.RELAY_NOTIFY_DISCONNECT, false,
+                                new MNotifyDisconnected()
+                                {
+                                    DisconnectedID = m.ID,
+                                    Reason = (ban) ? DisconnectType.BAN : DisconnectType.KICK
+                                }), true);
+                            DisconnectConnection(found, DisconnectReason.END);
+                            break;
+                        }
+                    case RelayMessage.RELAY_ADMIN_POWER_OFF:
+                        {
+                            throw new NotImplementedException();
+                            break;
+                        }
+                    case RelayMessage.RELAY_ADMIN_SET_PASSWORD:
+                        {
+                            var m = DeserializeStruct<MSetPassword>(msg.Data);
+                            _adminPassword = m.password;
+                            break;
+                        }
+                    case RelayMessage.RELAY_CLIENT_GET_STATUS:
+                        {
+                            QueueSendMessage(BasicMessage(con, (int)RelayMessage.RELAY_GET_MY_STATUS,
+                            true, new MStatus
+                            {
+                                isadmin = con == _adminConnection,
+                                islogged = LoggedIn.Contains(con),
+                            }), true);
+                            break;
+                        }
+                    default:
+                        {
+                            return MessageReaction.MALFORMED;
+                        }
+                }
+            }
+            catch (DeserializationFailedException)
+            {
+                return MessageReaction.ERROROUS;
+            }
+            return MessageReaction.ACCEPTED;
+        }
+        protected override MessageReaction ProcessMessage(IPEndPoint source, Message msg, Connection con)
+        {
+            if (!IsAllowedToMessage(msg, con))
+                return MessageReaction.ERROROUS;
+            if (msg.ReceiverID == ID_RELAY || MatchIDs(ID, msg.ReceiverID))
+            {
+                if (msg.MType > (int)MessageType.MESSAGE_TYPE_BEFORE_FIRST && msg.MType < (int)MessageType.MESAGE_TYPE_AFTER_LAST)
+                    return base.ProcessMessage(source, msg, con);
+                return ProcessRelayMessage(msg, con);
+            }
+            if (msg.ReceiverID == ID_UNKNOWN)
+            {
+                LogEvent("Attempting to relay to ID_UNKNOWN " + MessageInfo(msg), Severity.ERROR);
+                return MessageReaction.ERROROUS;
+            }
+            if (msg.ReceiverID == ID_ALL)
+            {
+                RelayToAll(con, msg);
+                return MessageReaction.ACCEPTED;
+            }
+            int nsent = 0;
+            foreach (var it in _loggedIn.FindAll(it => it.ID != msg.SenderID && MatchIDs(it.ID, msg.ReceiverID)))
+            {
+                RelayMessageTo(msg, it, msg.SenderID);
+                nsent++;
+            }
+            if (nsent == 0)
+            {
+                LogEvent($"Message that matched no connections ID: {msg.MessageID} FROM: {msg.SenderID} TO: {msg.ReceiverID}",
+                    Severity.ERROR);
+                return MessageReaction.ERROROUS;
+            }
+            return (nsent > 0) ? MessageReaction.ACCEPTED : MessageReaction.REJECTED;
+
+        }
+        protected override MessageReaction CheckAccepted(IPEndPoint source, Message msg, Connection con)
+        {
+            var br = base.CheckAccepted(source, msg, con);
+            if (br == MessageReaction.MALFORMED || br == MessageReaction.ERROROUS)
+                return br;
+            if (msg.MType != (int)MessageType.MSG_CONFIRM)
+                if (msg.MType > (int)MessageType.MESSAGE_TYPE_BEFORE_FIRST && msg.MType < (int)MessageType.MESAGE_TYPE_AFTER_LAST)
+                    return br;
+            Assert(con != null);
+            if (!IsAllowedToMessage(msg, con))
+                return MessageReaction.ERROROUS;
+            if (msg.MType > (int)RelayMessage.RELAY_BEFORE_FIRST && msg.MType < (int)RelayMessage.RELAY_AFTER_LAST)
+            {
+                if (msg.ReceiverID == ID_RELAY)
+                    return MessageReaction.ACCEPTED;
+                else
+                {
+                    LogEvent($@"A message in the relay range addressed to someone else received.
+                        ID: {msg.MessageID} FROM: {msg.SenderID} TO: {msg.ReceiverID} 
+                        TYPE: {msg.MType}.", Severity.ERROR);
+                    return MessageReaction.MALFORMED;
+                }
+            }
+            if (msg.ReceiverID == ID_ADMIN)
+                return MessageReaction.ACCEPTED;
+            if (msg.ReceiverID == ID_ALL)
+                return MessageReaction.ACCEPTED;
+            if (Connections.Any(it => MatchIDs(it.ID, msg.ReceiverID)))
+                return MessageReaction.ACCEPTED;
+            return MessageReaction.REJECTED;
+        }
+        protected override void OnFailure(NetException ex)
+        {
+            FailureShutdown();
+            _failHandle(ex);
+        }
+        protected override void OnLog(string msg, Severity s)
+        {
+            Console.WriteLine(s.ToString() + ": " + msg);
+        }
+
+        public RelayServer(ushort port, Action<NetException> _failHandle, string adminpassword, string clientpassword, bool start = false) : base(port, false)
+        {
+            this._failHandle = _failHandle;
+            this._adminPassword = adminpassword;
+            this._clientPassword = clientpassword;
+            this.ID = ID_RELAY;
+            if (start)
+                Start();
+        }
+    }
+
+}
