d148e469e4d10e85a7728d0fdec2a0adf7ab31c2
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2021-12-12 23:03:57 +0100

    Added basic exceptions handling and some refactoring inside AudioPlayer.cs

--- Audio/AudioPlayer.cs
+++ Audio/AudioPlayer.cs
@@ -31,51 +31,54 @@ public class AudioPlayer : Node
 	public override void _Ready()
 	{		
 		try{
-		USER_PREFERENCES_AUTOPLAY = true;
-		USER_PREFERENCES_MUSIC_VOLUME = 0.0f;
-		USER_PREFERENCES_SOUNDS_VOLUME = 0.0f;
-		
-		
-		
-		_introMusic = (AudioStreamPlayer)GetNode("MusicPlayer").GetNode("IntroMusic");
-		
-		_tileRotationSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("TileRotationAvailableSound");
-		_tileRotationDisabledSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("TileRotationDisabledSound");
-		_tilePlacedSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("TilePlacedSound");
-		_tileOverSpotSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("TileOverSpotSound");
-		
-		_meeplePlacedSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("MeeplePlacedSound");
-		
-		_songsList = new List<string>(){
-			"res://Audio/Music/2019-06-14_-_Warm_Light_-_David_Fesliyan.mp3",
-			"res://Audio/Music/2019-07-26_-_Healing_Water_FesliyanStudios.com_-_David_Renda.mp3",
-			"res://Audio/Music/2019-07-29_-_Elven_Forest_-_FesliyanStudios.com_-_David_Renda.mp3",
-			"res://Audio/Music/2020-02-22_-_Relaxing_Green_Nature_-_David_Fesliyan.mp3",
-			"res://Audio/Music/2020-06-18_-_Cathedral_Ambience_-_www.FesliyanStudios.com_David_Renda.mp3",
-			"res://Audio/Music/2021-06-03_-_Irish_Sunset_-_www.FesliyanStudios.com.mp3",
-			"res://Audio/Music/2021-06-03_-_Rolling_Hills_Of_Ireland_-_www.FesliyanStudios.com.mp3",
-			"res://Audio/Music/bensound-epic.mp3",
-			"res://Audio/Music/bensound-instinct.mp3",
-		};
+			USER_PREFERENCES_AUTOPLAY = true;
+			USER_PREFERENCES_MUSIC_VOLUME = 0.0f;
+			USER_PREFERENCES_SOUNDS_VOLUME = 0.0f;
+			
+			
+			
+			_introMusic = (AudioStreamPlayer)GetNode("MusicPlayer").GetNode("IntroMusic");
+			
+			_tileRotationSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("TileRotationAvailableSound");
+			_tileRotationDisabledSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("TileRotationDisabledSound");
+			_tilePlacedSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("TilePlacedSound");
+			_tileOverSpotSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("TileOverSpotSound");
+			
+			_meeplePlacedSound = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode("MeeplePlacedSound");
+			
+			_songsList = new List<string>(){
+				"res://Audio/Music/2019-06-14_-_Warm_Light_-_David_Fesliyan.mp3",
+				"res://Audio/Music/2019-07-26_-_Healing_Water_FesliyanStudios.com_-_David_Renda.mp3",
+				"res://Audio/Music/2019-07-29_-_Elven_Forest_-_FesliyanStudios.com_-_David_Renda.mp3",
+				"res://Audio/Music/2020-02-22_-_Relaxing_Green_Nature_-_David_Fesliyan.mp3",
+				"res://Audio/Music/2020-06-18_-_Cathedral_Ambience_-_www.FesliyanStudios.com_David_Renda.mp3",
+				"res://Audio/Music/2021-06-03_-_Irish_Sunset_-_www.FesliyanStudios.com.mp3",
+				"res://Audio/Music/2021-06-03_-_Rolling_Hills_Of_Ireland_-_www.FesliyanStudios.com.mp3",
+				"res://Audio/Music/bensound-epic.mp3",
+				"res://Audio/Music/bensound-instinct.mp3",
+			};
 
-		_currentSong = (AudioStreamPlayer)GetNode("MusicPlayer").GetNode("CurrentSong");
-		_currentSongIndex = 0 ;
-		_random = new Random();
-		_currentSong.Stream = _songStream;
+			_currentSong = (AudioStreamPlayer)GetNode("MusicPlayer").GetNode("CurrentSong");
+			_currentSongIndex = 0 ;
+			_random = new Random();
+			_currentSong.Stream = _songStream;
 
-		_muteLevel = -55.0f;
+			_muteLevel = -55.0f;
 
-		_lastMusicVolume = USER_PREFERENCES_MUSIC_VOLUME;
-		_lastSoundsVolume = USER_PREFERENCES_SOUNDS_VOLUME;
+			_lastMusicVolume = USER_PREFERENCES_MUSIC_VOLUME;
+			_lastSoundsVolume = USER_PREFERENCES_SOUNDS_VOLUME;
 		} catch (NullReferenceException e){
 			System.Diagnostics.Debug.WriteLine("Exception caught!. There is no such a node. "+e.Message);
 		} catch (Exception e){
-			System.Diagnostics.Debug.WriteLine("Exception caught: "+e.Message);
+			System.Diagnostics.Debug.WriteLine("AudioPlayer Exception: "+e.Message);
 		}
 	}
 
 	public void PlayNextSong(){
 		try{
+			if(_songsList.Count < 1)
+				throw new OperationCanceledException("There are no songs found on the playlist.");
+
 			if(USER_PREFERENCES_AUTOPLAY == true){
 				if((_introMusic.Playing == true)||(_currentSongIndex >= _songsList.Count -1)){
 					_currentSongIndex = 0; 	
@@ -87,9 +90,9 @@ public class AudioPlayer : Node
 				_currentSong.Play();
 			}
 		} catch(NullReferenceException e){
-			System.Diagnostics.Debug.WriteLine("Exception caught!. There is no such a node. "+e.Message);
+			System.Diagnostics.Debug.WriteLine("AudioPlayer Exception: "+e.Message);
 		} catch(ArgumentOutOfRangeException e){
-			System.Diagnostics.Debug.WriteLine("EXCEPTION CAUGHT! There is no song with given index! Skipping to first song...");
+			System.Diagnostics.Debug.WriteLine("AudioPlayer Excetpion! There is no song with given index! Skipping to first song...");
 			System.Diagnostics.Debug.WriteLine("EXCEPTION MESSAGE: " + e.Message);
 			_currentSongIndex = 0;
 		}
@@ -106,7 +109,6 @@ public class AudioPlayer : Node
 		_introMusic.Stop();
 	}
 	
-
 	public void PlaySound(string namedStreamPlayer){
 		try{
 			AudioStreamPlayer _soundToPlay = (AudioStreamPlayer)GetNode("SoundsPlayer").GetNode(namedStreamPlayer);
@@ -119,11 +121,7 @@ public class AudioPlayer : Node
 
 	public void SetAudioBusVolume(string audioBus, float value){
 		float volumeToSet = -_muteLevel*value*0.01f+_muteLevel;
-		if(volumeToSet <= _muteLevel){
-			volumeToSet = _muteLevel;
-		} else if(volumeToSet >= 0){
-			volumeToSet = 0 ;
-		}
+		volumeToSet = verifyVolumeLevel(volumeToSet);
 		try{
 			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex(audioBus),volumeToSet);				
 		} catch(NullReferenceException e){
@@ -132,39 +130,26 @@ public class AudioPlayer : Node
 		}
 	}
 
-	private void _setLastAudioVolume(string audioBus, float volume){
-		switch(audioBus){
-			case "Music":
-				_lastMusicVolume = volume;
-				break;
-			case "Sounds":
-				_lastSoundsVolume = volume;
-				break;
-			default:
-				break;
-		}
-	}
-
-	private float _getLastAudioVolume(string audioBus){
-		switch(audioBus){
-			case "Music":
-				return _lastMusicVolume;
-			case "Sounds":
-				return _lastSoundsVolume;
-			default:
-				return 0;
-		}
+	private float verifyVolumeLevel(float volume){
+		if(volume < _muteLevel)
+			return _muteLevel;
+		if(volume > 0)
+			return 0.0f;
+		return volume;
 	}
 
 	public void ToggleAudioBusVolume(string audioBus){
-		float lastVolume = _getLastAudioVolume(audioBus);
-		
-		float currentSoundsVolume = AudioServer.GetBusVolumeDb(AudioServer.GetBusIndex(audioBus));
-		if(currentSoundsVolume <= _muteLevel){
-			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex(audioBus),lastVolume);
-		} else {
-			_setLastAudioVolume(audioBus,currentSoundsVolume);
-			AudioServer.SetBusVolumeDb(AudioServer.GetBusIndex(audioBus),_muteLevel);
+		try{
+			int audioBusIndex = AudioServer.GetBusIndex(audioBus);
+			if(audioBusIndex < 0)
+				throw new ArgumentException("AudioPlayer Exception: Unknown AudioBus '" + audioBus +"'");
+			if(AudioServer.IsBusMute(audioBusIndex))
+				AudioServer.SetBusMute(audioBusIndex,false);
+			else
+				AudioServer.SetBusMute(audioBusIndex,true);
+
+		} catch (Exception e) {
+			System.Diagnostics.Debug.WriteLine(e.Message);
 		}
 	}	
 	// singnals handling
