35d08b1b9d1e7e2e23e88ae8844bb8d4897bd7b1
DESKTOP-K94QVJ7\choja <adam.chojaczyk@epicvr.pl>
2022-01-10 11:12:29 +0100

    Implemented playin different sound depending on placed meeple and main menu sounds: background music and hover-button 'cliks'.

--- Game/MainMenu/MainMenu.cs
+++ Game/MainMenu/MainMenu.cs
@@ -3,36 +3,50 @@ using System;
 
 
 public class MainMenu : Control
-{
-    Button _play;
-    Button _quit;
+{	
+	AudioPlayer _gameAudio;
+	
+	Button _play;
+	Button _quit;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        _play = GetNode<Button>("Play");
-        _play.Connect("pressed", this, nameof(OnPlayPressed));
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_gameAudio = GetNode<AudioPlayer>("/root/AudioPlayer");
+		_gameAudio.PlayMainMenuMusic(0);	
+		
+		_play = GetNode<Button>("Play");
+		_play.Connect("pressed", this, nameof(OnPlayPressed));
 
-        _quit = GetNode<Button>("Quit");
-        _quit.Connect("pressed", this, nameof(OnQuitPressed));
-    }
+		_quit = GetNode<Button>("Quit");
+		_quit.Connect("pressed", this, nameof(OnQuitPressed));
+	}
 
-    void OnPlayPressed()
-    {
-        GD.Print("Play pressed!");
-        GetTree().ChangeScene("res://Game/Loading/MainMenuLoad.tscn");
-    }
+	void OnPlayPressed()
+	{
+		GD.Print("Play pressed!");		
+		GetTree().ChangeScene("res://Game/Loading/MainMenuLoad.tscn");
+	}
 
-    void OnQuitPressed()
-    {
-        GD.Print("Quit pressed!");
-        GetTree().Quit();
-    }
+	void OnQuitPressed()
+	{
+		GD.Print("Quit pressed!");
+		GetTree().Quit();
+	}
 
+	private void _onPlayMouseEntered()
+	{
+		_gameAudio.PlaySound("TileOverSpotSound");
+	}
+	
+	private void _onQuitMouseEntered()
+	{
+		_gameAudio.PlaySound("TileOverSpotSound");
+	}
 
   // Called every frame. 'delta' is the elapsed time since the previous frame.
   public override void _Process(float delta)
   {
-      
+	  
   }
 }
