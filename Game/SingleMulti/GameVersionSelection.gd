1bd7bee72242f2fec04db8b3db46f012eaf8cc7b
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-04-19 22:34:25 +0200

    gd script: moving frame

---
+++ Game/SingleMulti/GameVersionSelection.gd
@@ -0,0 +1,37 @@
+extends Sprite
+
+
+var gameVersions = []
+var currentSelected = 0		# Spot of the FrameSelector within the gameVersion[]
+var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
+var currentRowSpot = 0
+
+export (Texture) var selectorTexture
+export (int) var amountOfRows = 1      # The total amount of rows the character select is able to show 
+export (Vector2) var paintingOffset
+
+onready var gridContainer = get_parent().get_node("GridContainer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for nameOfGameVersion in get_tree().get_nodes_in_group("GameVersions"):
+		gameVersions.append(nameOfGameVersion)
+	
+	texture = selectorTexture
+	
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_RIGHT:
+			if(currentColumnSpot < gridContainer.columns - 1):
+				print("KEY_RIGHT was pressed")
+				currentSelected += 1
+				currentColumnSpot += 1
+				position.x += paintingOffset.x
+			
+		elif event.scancode == KEY_LEFT:
+			if(currentColumnSpot > 0):
+				print("KEY_LEFT was pressed")
+				currentSelected -= 1
+				currentColumnSpot -= 1
+				position.x -= paintingOffset.x
+	
