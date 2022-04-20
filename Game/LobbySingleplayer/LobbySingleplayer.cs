61547a3ff7bac882e5490c590d57c1dd9574a04a
Ewelina Walkusz <65917010+ewe-ina@users.noreply.github.com>
2022-04-20 22:31:18 +0200

    GUI single multi (#48)
    * single/multi - graphic elements
    
    * gd script: moving frame
    
    * singleplayer selection

---
+++ Game/LobbySingleplayer/LobbySingleplayer.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.PortableExecutable;
+using System.Runtime;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using ExtraMath;
+using Godot;
+using Newtonsoft.Json;
+using static System.Math;
+using static Utils;
+using Expression = System.Linq.Expressions.Expression;
+using Thread = System.Threading.Thread;
+
+public class LobbySingleplayer : Control
+{
+    Button _play;
+    Button _quit;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _play = GetNode<Button>("Play");
+        _play.Connect("pressed", this, nameof(OnPlayPressed));
+
+        _quit = GetNode<Button>("Quit");
+        _quit.Connect("pressed", this, nameof(OnQuitPressed));
+    }
+
+    void OnPlayPressed()
+    {
+        GD.Print("Play pressed!");
+    }
+
+    void OnQuitPressed()
+    {
+        GD.Print("Quit pressed!");
+        GetTree().Quit();
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+
+    }
+}
---
+++ Game/SingleMulti/GameVersionSelection.gd
@@ -0,0 +1,45 @@
+extends Sprite
+
+
+var gameVersions = []
+var currentSelected = 0		# Spot of the FrameSelector within the gameVersion[]
+var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
+var currentRowSpot = 0
+
+export (Texture) var selectorTexture
+export (int) var amountOfRows = 1      # The total amount of rows the character select is able to show 
+export (Vector2) var paintingOffset
+
+onready var gridContainer = get_parent().get_node("GridContainer")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for nameOfGameVersion in get_tree().get_nodes_in_group("GameVersions"):
+		gameVersions.append(nameOfGameVersion)
+	
+	texture = selectorTexture
+	
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_RIGHT:
+			if(currentColumnSpot < gridContainer.columns - 1):
+				print("KEY_RIGHT was pressed")
+				currentSelected += 1
+				currentColumnSpot += 1
+				position.x += paintingOffset.x
+			
+		elif event.scancode == KEY_LEFT:
+			if(currentColumnSpot > 0):
+				print("KEY_LEFT was pressed")
+				currentSelected -= 1
+				currentColumnSpot -= 1
+				position.x -= paintingOffset.x
+				
+		elif event.scancode == KEY_ENTER:
+			print("KEY_ENTER was pressed")
+			if gameVersions[currentSelected].name == "Single":
+				print("go to LobbySingleplayer")
+				get_tree().change_scene("res://Game/LobbySingleplayer/LobbySingleplayer.tscn")
+			elif gameVersions[currentSelected].name == "Multi":
+				print("go to LobbyMultiplayer")
+				#get_tree...
