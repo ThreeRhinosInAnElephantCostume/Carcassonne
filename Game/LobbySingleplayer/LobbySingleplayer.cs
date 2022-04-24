7e583421e08c61ee1be8f4d96aa6b247dfa8e955
Ewelina Walkusz <65917010+ewe-ina@users.noreply.github.com>
2022-04-24 18:04:49 +0200

    GUI lobby singleplayer (#58)
    * some notes
    
    * visibility of bots
    
    * wip
    
    * adding bots to the game

--- Game/LobbySingleplayer/LobbySingleplayer.cs
+++ Game/LobbySingleplayer/LobbySingleplayer.cs
@@ -26,6 +26,27 @@ public class LobbySingleplayer : Control
     Button _play;
     Button _quit;
 
+    int _amountOfBots = 4;  // zmienna, która będzie przechwytywana z popup'a
+
+    enum BotLevel
+    {
+        Easy,
+        Mid,
+        Hard
+    }
+
+
+    List<TextureRect> _bots = new List<TextureRect>();
+
+
+    // TODO: popup: (Enter your name: . How many opponents do you want? ) zczytana liczba przeciwników, min 1 max 4 (jeśli mniej niż 1, ustaw 1, jeśli więcej niż 4 ustaw 4)
+    // TODO: liczba botów przekazana do _amountOfBots
+    // TODO: nazwa gracza przekazana do Label gracza
+    // TODO: odświeżenie sceny
+    //
+    // black -> blue -> yellow -> green
+    // 
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
@@ -34,23 +55,100 @@ public class LobbySingleplayer : Control
 
         _quit = this.GetNodeSafe<Button>("Quit");
         _quit.OnButtonPressed(OnQuitPressed);
+
+        // widoczne boty tylko w liczbie amountOfBots
+        PrepareBots(_amountOfBots);
+        BotsVisibilityOn();
     }
 
     void OnPlayPressed()
     {
-        var aitheme = Globals.PersonalThemes["red"].Copy();
-        aitheme.IconPath = "res://GUI/avatars/avatarbot3.png";
-        aitheme.AvatarPath = "res://GUI/avatars/avatarbot3.png";
+        var blacktheme = Globals.PersonalThemes["black"].Copy();
+        var bluetheme = Globals.PersonalThemes["blue"].Copy();
+        var yellowtheme = Globals.PersonalThemes["yellow"].Copy();
+        var greentheme = Globals.PersonalThemes["green"].Copy();
+        
         var generators = new List<Game.AgentGenerator>()
         {
-            (g, e, i, p, rng) => new Game.GameLocalAgent(g, $"Player", p, Globals.PersonalThemes["blue"].Copy()),
-            (g, e, i, p, rng) => new Game.GameAIAgent(g, $"AI", p, new AI.RandomAI(new RNG(rng.NextULong())), aitheme),
+            (g, e, i, p, rng) => new Game.GameLocalAgent(g, $"Player", p, Globals.PersonalThemes["red"].Copy()),
         };
+
+        // choose and add bots
+        ChooseBot(blacktheme, BotLevel.Easy, generators);
+        if(_amountOfBots > 1)
+            ChooseBot(bluetheme, BotLevel.Easy, generators);
+        if(_amountOfBots > 2)    
+            ChooseBot(yellowtheme, BotLevel.Easy, generators);
+        if(_amountOfBots > 3)   
+            ChooseBot(greentheme, BotLevel.Easy, generators);
+
         var ui = (InGameUI)Globals.Scenes.InGameUIPacked.Instance();
         var game = Game.NewLocalGame(ui, generators, "BaseGame/BaseTileset.json", 666);
         ui.SetGame(game);
         GetTree().Root.AddChild(ui);
         DestroyNode(this);
+    }
+
+    // choose and add bot to the game
+    void ChooseBot(PersonalTheme theme, BotLevel level, List<Game.AgentGenerator> generator)
+    {
+        var avatarbotPath = "";
+        Game.AgentGenerator bot = null; 
+        switch (level)
+        {
+            case BotLevel.Easy:
+                avatarbotPath = "res://GUI/avatars/avatarbot3.png";
+                theme.IconPath = avatarbotPath;
+                theme.AvatarPath = avatarbotPath;
+                bot = (g, e, i, p, rng) => new Game.GameAIAgent(g, $"AI", p, new AI.RandomAI(new RNG(rng.NextULong())), theme);
+                break;
+            case BotLevel.Mid:
+                avatarbotPath = "res://GUI/avatars/avatarbot2.png";
+                theme.IconPath = avatarbotPath;
+                theme.AvatarPath = avatarbotPath;
+                bot = (g, e, i, p, rng) => new Game.GameAIAgent(g, $"AI", p, new AI.RandomAI(new RNG(rng.NextULong())), theme); //zmienić na AI.Medium
+                break;
+            case BotLevel.Hard:
+                avatarbotPath = "res://GUI/avatars/avatarbot1.png";
+                theme.IconPath = avatarbotPath;
+                theme.AvatarPath = avatarbotPath;
+                bot = (g, e, i, p, rng) => new Game.GameAIAgent(g, $"AI", p, new AI.RandomAI(new RNG(rng.NextULong())), theme); //zmienić na AI.Hard
+                break;
+            default:
+                avatarbotPath = "res://GUI/avatars/avatarbot3.png";
+                theme.IconPath = avatarbotPath;
+                theme.AvatarPath = avatarbotPath;
+                bot = (g, e, i, p, rng) => new Game.GameAIAgent(g, $"AI", p, new AI.RandomAI(new RNG(rng.NextULong())), theme);
+                break;
+        }
+                
+        generator.Add(bot);
+    }
+
+    void PrepareBots(int amountOfBots)
+    {
+        if(amountOfBots > 1)
+            BotsAdd("Blue");
+        if(amountOfBots > 2)    
+            BotsAdd("Yellow");
+        if(amountOfBots > 3)   
+            BotsAdd("Green");
+    }
+
+    void BotsAdd(string color)
+    {
+        _bots.Add(this.GetNodeSafe<TextureRect>($"GridContainer/GridContainerBots/HBoxContainerBotsEasy/BotEasy{color}"));
+        _bots.Add(this.GetNodeSafe<TextureRect>($"GridContainer/GridContainerBots/HBoxContainerBotsMid/BotMid{color}"));
+        _bots.Add(this.GetNodeSafe<TextureRect>($"GridContainer/GridContainerBots/HBoxContainerBotsHard/BotHard{color}"));
+    }
+
+    void BotsVisibilityOn()
+    {
+        //_playerContainers.ForEach(this.Visible = false);
+        for (int i = 0; i < _bots.Count; i++)
+        {
+            _bots[i].Visible = true;
+        }
     }
 
     void OnQuitPressed()
--- Game/LobbySingleplayer/Selector1.gd
+++ Game/LobbySingleplayer/Selector1.gd
@@ -6,11 +6,18 @@ var currentSelected = 0		# Spot of the FrameSelector within the botLevels_1[]
 var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
 var currentRowSpot = 0
 
+var botLevelsNames = ["Easy bot", "Mid", "Hard"]
+
+
 export (Texture) var selectorTexture
 export (int) var amountOfRows = 3      # The total amount of rows the character select is able to show 
 export (Vector2) var avatarsOffset
+#export (Label) var selectorLabel_1 = "Easy bot"
 
 onready var gridContainer = get_parent().get_node("GridContainer")
+
+onready var selector1 = get_parent().get_node("Selector1")
+onready var selectorLabel1 = selector1.get_node("Label")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -19,8 +26,17 @@ func _ready():
 	
 	texture = selectorTexture
 	
+	selectorLabel1.text = "Easy bot"
+	print(selectorLabel1.text)
+	
+	
+		
+	
+	
 func _input(event):
 	if event is InputEventKey and event.pressed:
+		
+		
 		if event.scancode == KEY_UP:
 			if(currentRowSpot > 0):
 				print("KEY_UP was pressed")
@@ -34,3 +50,11 @@ func _input(event):
 				currentSelected += 1
 				currentRowSpot += 1
 				position.y += avatarsOffset.y
+				
+		if currentSelected == 0:
+			selectorLabel1.text = "Easy bot"
+		elif currentSelected == 1:
+			selectorLabel1.text = "Mid bot"
+		elif currentSelected == 2:
+			selectorLabel1.text = "Hard bot"
+		print(selectorLabel1.text)
