018adfb8fc2e7e4a4fd06b290a763d97486576f1
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-04-29 22:46:45 +0200

    add Selector2 script

--- Game/LobbySingleplayer/Selector2.gd
+++ Game/LobbySingleplayer/Selector2.gd
@@ -1,11 +1,12 @@
 extends Sprite
 
 
-var botLevels_1 = []
+var botLevels_2 = []
 var currentSelected = 0		# Spot of the FrameSelector within the botLevels_1[]
 var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
 var currentRowSpot = 0
 var enterPressed = false
+var previousSelected = false
 
 var botLevelsNames = ["Easy bot", "Mid", "Hard"]
 
@@ -14,31 +15,30 @@ export (Texture) var selectorTexture
 export (int) var amountOfRows = 3      # The total amount of rows the character select is able to show 
 export (Vector2) var avatarsOffset
 export (int) var botBlackLevel
-#export (Label) var selectorLabel_1 = "Easy bot"
 
 onready var gridContainer = get_parent().get_node("GridContainer")
 
-onready var selector1 = get_parent().get_node("Selector1")
-onready var selectorLabel1 = selector1.get_node("Label")
-onready var nextSelector = get_node("../Selector2")
-onready var botBlue = get_node("/root/LobbySingle/GridContainer/GridContainerBots/HBoxContainerBotsEasy/BotEasyBlue")
+onready var selector2 = get_node("../Selector2")
+onready var selectorLabel2 = selector2.get_node("Label")
+onready var nextSelector = get_node("../Selector3")
+onready var botYellow = get_node("/root/LobbySingle/GridContainer/GridContainerBots/HBoxContainerBotsEasy/BotEasyYellow")
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for nameOfBotLevels_1 in get_tree().get_nodes_in_group("BotLevels_1"):
-		botLevels_1.append(nameOfBotLevels_1)
+	for nameOfBotLevels_2 in get_tree().get_nodes_in_group("BotLevels_2"):
+		botLevels_2.append(nameOfBotLevels_2)
 	
 	texture = selectorTexture
 	
-	selectorLabel1.text = "Easy bot"
-	print(selectorLabel1.text)
+	selectorLabel2.text = "Easy bot"
+	print(selectorLabel2.text)
 	
-	
-		
+	print(previousSelected)
 	
 	
 func _input(event):
-	if event is InputEventKey and event.pressed and not enterPressed:
+	if event is InputEventKey and event.pressed and previousSelected and not enterPressed:
 		
 		
 		if event.scancode == KEY_UP:
@@ -56,18 +56,17 @@ func _input(event):
 				position.y += avatarsOffset.y
 				
 		if currentSelected == 0:
-			selectorLabel1.text = "Easy bot"
+			selectorLabel2.text = "Easy bot"
 		elif currentSelected == 1:
-			selectorLabel1.text = "Mid bot"
+			selectorLabel2.text = "Mid bot"
 		elif currentSelected == 2:
-			selectorLabel1.text = "Hard bot"
-		print(selectorLabel1.text)
+			selectorLabel2.text = "Hard bot"
+		print(selectorLabel2.text)
 		
 		if event.scancode == KEY_ENTER:
 			enterPressed = true
 			botBlackLevel = currentSelected
 			print(botBlackLevel)
-			nextSelector.previousSelected = true
 			
-			if botBlue.visible == true:
+			if botYellow.visible == true:
 				nextSelector.visible = true
