432614a5ba065fd315256abcbb401b593709e773
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-04-29 22:55:39 +0200

    add Selector3 script

--- Game/LobbySingleplayer/Selector3.gd
+++ Game/LobbySingleplayer/Selector3.gd
@@ -1,7 +1,7 @@
 extends Sprite
 
 
-var botLevels_2 = []
+var botLevels_3 = []
 var currentSelected = 0		# Spot of the FrameSelector within the botLevels_1[]
 var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
 var currentRowSpot = 0
@@ -14,25 +14,25 @@ var botLevelsNames = ["Easy bot", "Mid", "Hard"]
 export (Texture) var selectorTexture
 export (int) var amountOfRows = 3      # The total amount of rows the character select is able to show 
 export (Vector2) var avatarsOffset
-export (int) var botBlueLevel
+export (int) var botYellowLevel
 
 onready var gridContainer = get_parent().get_node("GridContainer")
 
-onready var selector2 = get_node("../Selector2")
-onready var selectorLabel2 = selector2.get_node("Label")
-onready var nextSelector = get_node("../Selector3")
-onready var botYellow = get_node("/root/LobbySingle/GridContainer/GridContainerBots/HBoxContainerBotsEasy/BotEasyYellow")
+onready var selector3 = get_node("../Selector3")
+onready var selectorLabel3 = selector3.get_node("Label")
+onready var nextSelector = get_node("../Selector4")
+onready var botGreen = get_node("/root/LobbySingle/GridContainer/GridContainerBots/HBoxContainerBotsEasy/BotEasyGreen")
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for nameOfBotLevels_2 in get_tree().get_nodes_in_group("BotLevels_2"):
-		botLevels_2.append(nameOfBotLevels_2)
+	for nameOfBotLevels_3 in get_tree().get_nodes_in_group("BotLevels_3"):
+		botLevels_3.append(nameOfBotLevels_3)
 	
 	texture = selectorTexture
 	
-	selectorLabel2.text = "Easy bot"
-	print(selectorLabel2.text)
+	selectorLabel3.text = "Easy bot"
+	print(selectorLabel3.text)
 	
 	print(previousSelected)
 	
@@ -56,18 +56,17 @@ func _input(event):
 				position.y += avatarsOffset.y
 				
 		if currentSelected == 0:
-			selectorLabel2.text = "Easy bot"
+			selectorLabel3.text = "Easy bot"
 		elif currentSelected == 1:
-			selectorLabel2.text = "Mid bot"
+			selectorLabel3.text = "Mid bot"
 		elif currentSelected == 2:
-			selectorLabel2.text = "Hard bot"
-		print(selectorLabel2.text)
+			selectorLabel3.text = "Hard bot"
+		print(selectorLabel3.text)
 		
 		if event.scancode == KEY_ENTER:
 			enterPressed = true
-			botBlueLevel = currentSelected
-			print(botBlueLevel)
-			nextSelector.previousSelected = true
+			botYellowLevel = currentSelected
+			print(botYellowLevel)
 			
-			if botYellow.visible == true:
+			if botGreen.visible == true:
 				nextSelector.visible = true
