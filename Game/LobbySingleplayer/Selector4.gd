f1993017d65e9cfce1f4d1d4b3eff8c098079565
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-04-29 23:01:51 +0200

    add Selector4 script

---
+++ Game/LobbySingleplayer/Selector4.gd
@@ -0,0 +1,69 @@
+extends Sprite
+
+
+var botLevels_4 = []
+var currentSelected = 0		# Spot of the FrameSelector within the botLevels_1[]
+var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
+var currentRowSpot = 0
+var enterPressed = false
+var previousSelected = false
+
+var botLevelsNames = ["Easy bot", "Mid", "Hard"]
+
+
+export (Texture) var selectorTexture
+export (int) var amountOfRows = 3      # The total amount of rows the character select is able to show 
+export (Vector2) var avatarsOffset
+export (int) var botGreenLevel
+
+onready var gridContainer = get_parent().get_node("GridContainer")
+
+onready var selector4 = get_node("../Selector4")
+onready var selectorLabel4 = selector4.get_node("Label")
+#onready var nextSelector = get_node("../Selector4")
+#onready var botGreen = get_node("/root/LobbySingle/GridContainer/GridContainerBots/HBoxContainerBotsEasy/BotEasyGreen")
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for nameOfBotLevels_4 in get_tree().get_nodes_in_group("BotLevels_4"):
+		botLevels_4.append(nameOfBotLevels_4)
+	
+	texture = selectorTexture
+	
+	selectorLabel4.text = "Easy bot"
+	print(selectorLabel4.text)
+	
+	print(previousSelected)
+	
+	
+func _input(event):
+	if event is InputEventKey and event.pressed and previousSelected and not enterPressed:
+		
+		
+		if event.scancode == KEY_UP:
+			if(currentRowSpot > 0):
+				print("KEY_UP was pressed")
+				currentSelected -= 1
+				currentRowSpot -= 1
+				position.y -= avatarsOffset.y
+			
+		elif event.scancode == KEY_DOWN:
+			if(currentRowSpot < amountOfRows - 1):
+				print("KEY_DOWN was pressed")
+				currentSelected += 1
+				currentRowSpot += 1
+				position.y += avatarsOffset.y
+				
+		if currentSelected == 0:
+			selectorLabel4.text = "Easy bot"
+		elif currentSelected == 1:
+			selectorLabel4.text = "Mid bot"
+		elif currentSelected == 2:
+			selectorLabel4.text = "Hard bot"
+		print(selectorLabel4.text)
+		
+		if event.scancode == KEY_ENTER:
+			enterPressed = true
+			botGreenLevel = currentSelected
+			print(botGreenLevel)
