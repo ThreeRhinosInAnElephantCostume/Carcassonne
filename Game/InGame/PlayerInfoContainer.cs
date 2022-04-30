e73413d2ce5ab02adf084dfd9181e00914773398
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-04-30 22:24:58 +0200

    note

--- Game/InGame/PlayerInfoContainer.cs
+++ Game/InGame/PlayerInfoContainer.cs
@@ -49,7 +49,7 @@ public class PlayerInfoContainer : ControlProp
     void RealUpdatePlayer()
     {
         Assert(_player != null);
-        _nMeeplesLabel.Text = $"{_player.Pawns.Count(it => it is Meeple)}";
+        _nMeeplesLabel.Text = $"{_player.Pawns.Count(it => it is Meeple)}"; // dlaczego nie aktualizuje się liczba meepli?
         _nPointsLabel.Text = $"{_player.Score}(+{_player.PotentialScore})";
         _playerNameLabel.Text = _agent.Name;
     }
