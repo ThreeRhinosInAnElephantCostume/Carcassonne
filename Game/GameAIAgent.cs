9b3100bd41c7179c1e7fadd3d9d4b02d172b109c
ThreeRhinosInAnElephantCostume <myrealstemail@protonmail.com>
2022-05-02 14:06:12 +0200

    Split off AI to another thread, implemented delay as a constant

--- Game/GameAIAgent.cs
+++ Game/GameAIAgent.cs
@@ -1,6 +1,4 @@
-﻿
-
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
@@ -8,6 +6,7 @@ using System.Linq.Expressions;
 using System.Reflection;
 using System.Runtime;
 using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using Carcassonne;
 using ExtraMath;
@@ -21,12 +20,24 @@ public partial class Game
     public class GameAIAgent : GameAgent
     {
         public AI.AIPlayer AI { get; protected set; }
-        public override async void OnTurn(GameEngine engine)
+        public override void OnTurn(GameEngine engine)
         {
             Assert(this._game.CurrentAgent.Player == this.Player);
-            await Task.Delay(1000);
-            AI.MakeMove(engine);
-            this._game.AgentExecuteImplied(_game.CurrentAgent);
+            ThreadPool.QueueUserWorkItem(o =>
+            {
+                DateTime start = DateTime.Now;
+                GameEngine clone;
+                clone = engine.Clone();
+                AI.MakeMove(clone);
+
+                // Ensure that at least Constants.MINIMUM_AI_DELAY_S has passed
+                var seconds = (DateTime.Now - start).TotalSeconds;
+                var dif = Constants.MINIMUM_AI_DELAY_S - seconds;
+                if (dif > 0)
+                    System.Threading.Thread.Sleep((int)(dif * 1000f));
+
+                this._game.AgentExecute(this, clone.History.Last());
+            }, null);
         }
         public GameAIAgent(Game game, string name, GameEngine.Player player, AI.AIPlayer AI, PersonalTheme theme) : base(game, name, PlayerType.AI, player, theme)
         {
