47d886c8e0887b51cc58188de34176229f51abd3
MartaSien <msienkiewicz7@gmail.com>
2022-05-03 12:30:22 +0200

    Reverted hard-coded mid AI

--- Game/LobbySingleplayer/LobbySingleplayer.cs
+++ Game/LobbySingleplayer/LobbySingleplayer.cs
@@ -64,7 +64,7 @@ public class LobbySingleplayer : Control
     void OnPlayPressed()
     {
         // Load levels of bots
-        BotLevel _botBlack = BotLevel.Mid;
+        BotLevel _botBlack = (BotLevel)_black;
         GD.Print($"Black bot is {_botBlack}");
         BotLevel _botBlue = (BotLevel)_blue;
         GD.Print($"Blue bot is {_botBlue}");
