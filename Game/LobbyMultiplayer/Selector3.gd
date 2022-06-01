da9fe677745937ff66cb667ee39dcde79c993f4d
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-06-01 22:59:41 +0200

    add Selector3

--- Game/LobbyMultiplayer/Selector3.gd
+++ Game/LobbyMultiplayer/Selector3.gd
@@ -1,8 +1,8 @@
 extends Sprite
 
 
-var avatars_2 = []
-var currentSelected = 0		# Spot of the FrameSelector within the avatars_2[]
+var avatars_3 = []
+var currentSelected = 0		# Spot of the FrameSelector within the avatars_3[]
 var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
 var currentRowSpot = 0
 var enterPressed = false
@@ -11,19 +11,19 @@ var previousSelected = false
 export (Texture) var selectorTexture
 export (int) var amountOfRows = 3      # The total amount of rows the character select is able to show 
 export (Vector2) var avatarsOffset
-export (int) var playerBlueAvatar
+export (int) var playerYellowAvatar
 
 onready var mainScript = get_parent()
 onready var gridContainer = get_node("../GridContainer")
-onready var selector2 = get_node("../Selector2")
-onready var selectorLabel2 = selector2.get_node("Label")
-onready var nextSelector = get_node("../Selector3")
-onready var avatarYellow = get_node("/root/LobbyMulti/GridContainer/GridContainerAvatars/HBoxContainerAvatar1/Avatar1Yellow")
+onready var selector3 = get_node("../Selector3")
+onready var selectorLabel3 = selector3.get_node("Label")
+onready var nextSelector = get_node("../Selector4")
+onready var avatarGreen = get_node("/root/LobbyMulti/GridContainer/GridContainerAvatars/HBoxContainerAvatar1/Avatar1Green")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for nameOfAvatars_2 in get_tree().get_nodes_in_group("Avatars_2"):
-		avatars_2.append(nameOfAvatars_2)
+	for nameOfAvatars_3 in get_tree().get_nodes_in_group("Avatars_3"):
+		avatars_3.append(nameOfAvatars_3)
 	
 	texture = selectorTexture
 
@@ -49,10 +49,10 @@ func _input(event):
 
 		if event.scancode == KEY_ENTER:
 			enterPressed = true
-			playerBlueAvatar = currentSelected
-			print(playerBlueAvatar)
+			playerYellowAvatar = currentSelected
+			print(playerYellowAvatar)
 			nextSelector.previousSelected = true
-			mainScript._blue = currentSelected
+			mainScript._yellow = currentSelected
 			
-			if avatarYellow.visible == true:
+			if avatarGreen.visible == true:
 				nextSelector.visible = true
