cdf5aa448b243f082ff3e6ab5d43cece48ca0ada
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-06-01 22:26:15 +0200

    pop up is ready

--- Game/LobbyMultiplayer/Dialog0.gd
+++ Game/LobbyMultiplayer/Dialog0.gd
@@ -1,6 +1,9 @@
 extends WindowDialog
 
 var players
+var HBox2
+var HBox3
+var HBox4
 
 onready var mainScript = get_parent()
 onready var selector0 = get_node("../Selector0")
@@ -28,26 +31,74 @@ func _ready():
 
 	
 func _on_Button_button_down():
+	
+	
+	var name0 = find_node("Name0").text.strip_edges()
+	var name1 = find_node("Name1").text.strip_edges()
+	var name2 = find_node("Name2").text.strip_edges()
+	var name3 = find_node("Name3").text.strip_edges()
+	var name4 = find_node("Name4").text.strip_edges()
+	print("___________")
+	print("imię gracza 1:")
+	print(name0)
+	print("imię gracza 2:")
+	print(name1)
+	print("imię gracza 3:")
+	print(name2)
+	print("imię gracza 4:")
+	print(name3)
+	print("imię gracza 5:")
+	print(name4)
+	print("___________")
+	if name0.empty():
+		name0 = "Player 1"
+	selectorLabel0.text = name0
+	if name1.empty():
+		name1 = "Player 2"
+	selectorLabel1.text = name1
+	if name2.empty():
+		name2 = "Player 3"
+	selectorLabel2.text = name2
+	if name3.empty():
+		name3 = "Player 4"
+	selectorLabel3.text = name3
+	if name4.empty():
+		name4 = "Player 5"
+	selectorLabel4.text = name4
+	
+	
+	hide()
+
+
+func _on_Ok_button_down():
+
 	players = find_node("Players").text.strip_edges()
 	print("___________")
 	print("Liczba graczy:")
 	print(players)
 	print("___________")
 	
-	var name0 = find_node("Name0").text.strip_edges()
-	print("___________")
-	print("imię:")
-	print(name0)
-	print("___________")
-	if name0.empty():
-		name0 = "Player"
-	selectorLabel0.text = name0
+	HBox2 = find_node("HBox2")
+	HBox3 = find_node("HBox3")
+	HBox4 = find_node("HBox4")
+	
+	if int(players) == 3:
+		HBox2.visible = true
+		HBox3.visible = false
+		HBox4.visible = false
+	elif int(players) == 4:
+		HBox2.visible = true
+		HBox3.visible = true
+		HBox4.visible = false
+	elif int(players) > 4:
+		HBox2.visible = true
+		HBox3.visible = true
+		HBox4.visible = true
+	else:
+		HBox2.visible = false
+		HBox3.visible = false
+		HBox4.visible = false
 	
 	mainScript._amountOfPlayers = players
 	#mainScript._namePlayer0 = name0
 	mainScript.PlayersVisibilityOn()
-	hide()
-	
-func _on_Name0_text_entered(_new_text):
-	_on_Button_button_down()
-	
