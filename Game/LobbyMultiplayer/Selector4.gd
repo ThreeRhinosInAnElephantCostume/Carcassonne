24c6c8cb64c3badb670ea60326232fe07c221be7
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-06-01 23:08:43 +0200

    add Selector4

---
+++ Game/LobbyMultiplayer/Selector4.gd
@@ -0,0 +1,53 @@
+extends Sprite
+
+
+var avatars_4 = []
+var currentSelected = 0		# Spot of the FrameSelector within the avatars_4[]
+var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
+var currentRowSpot = 0
+var enterPressed = false
+var previousSelected = false
+
+export (Texture) var selectorTexture
+export (int) var amountOfRows = 3      # The total amount of rows the character select is able to show 
+export (Vector2) var avatarsOffset
+export (int) var playerGreenAvatar
+
+onready var mainScript = get_parent()
+onready var gridContainer = get_node("../GridContainer")
+onready var selector4 = get_node("../Selector4")
+onready var selectorLabel4 = selector4.get_node("Label")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	for nameOfAvatars_4 in get_tree().get_nodes_in_group("Avatars_4"):
+		avatars_4.append(nameOfAvatars_4)
+	
+	texture = selectorTexture
+
+
+func _input(event):
+	if event is InputEventKey and event.pressed and previousSelected and not enterPressed:
+		
+		
+		if event.scancode == KEY_UP:
+			if(currentRowSpot > 0):
+				print("KEY_UP was pressed")
+				currentSelected -= 1
+				currentRowSpot -= 1
+				position.y -= avatarsOffset.y
+
+		elif event.scancode == KEY_DOWN:
+			if(currentRowSpot < amountOfRows - 1):
+				print("KEY_DOWN was pressed")
+				currentSelected += 1
+				currentRowSpot += 1
+				position.y += avatarsOffset.y
+				
+
+		if event.scancode == KEY_ENTER:
+			enterPressed = true
+			playerGreenAvatar = currentSelected
+			print(playerGreenAvatar)
+			mainScript._green = currentSelected
+			
