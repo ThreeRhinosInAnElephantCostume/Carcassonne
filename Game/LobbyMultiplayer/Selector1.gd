6f47adfa3e292f4553d710b2bc32c2f7f3442141
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-06-01 22:44:21 +0200

    add Selector1

--- Game/LobbyMultiplayer/Selector1.gd
+++ Game/LobbyMultiplayer/Selector1.gd
@@ -1,35 +1,35 @@
 extends Sprite
 
 
-var avatars_0 = []
-var currentSelected = 0		# Spot of the FrameSelector within the botLevels_1[]
+var avatars_1 = []
+var currentSelected = 0		# Spot of the FrameSelector within the avatars_1[]
 var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
 var currentRowSpot = 0
 var enterPressed = false
-
+var previousSelected = false
 
 export (Texture) var selectorTexture
 export (int) var amountOfRows = 3      # The total amount of rows the character select is able to show 
 export (Vector2) var avatarsOffset
-export (int) var playerRedAvatar
+export (int) var playerBlackAvatar
 
 onready var mainScript = get_parent()
 onready var gridContainer = get_node("../GridContainer")
-onready var selector0 = get_node("../Selector0")
+onready var selector0 = get_node("../Selector1")
 onready var selectorLabel0 = selector0.get_node("Label")
-onready var nextSelector = get_node("../Selector1")
-onready var avatarBlack = get_node("/root/LobbyMulti/GridContainer/GridContainerAvatars/HBoxContainerAvatar1/Avatar1Black")
+onready var nextSelector = get_node("../Selector2")
+onready var avatarBlue = get_node("/root/LobbyMulti/GridContainer/GridContainerAvatars/HBoxContainerAvatar1/Avatar1Blue")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for nameOfAvatars_0 in get_tree().get_nodes_in_group("Avatars_0"):
-		avatars_0.append(nameOfAvatars_0)
+	for nameOfAvatars_1 in get_tree().get_nodes_in_group("Avatars_1"):
+		avatars_1.append(nameOfAvatars_1)
 	
 	texture = selectorTexture
 
 
 func _input(event):
-	if event is InputEventKey and event.pressed and not enterPressed:
+	if event is InputEventKey and event.pressed and previousSelected and not enterPressed:
 		
 		
 		if event.scancode == KEY_UP:
@@ -49,10 +49,10 @@ func _input(event):
 
 		if event.scancode == KEY_ENTER:
 			enterPressed = true
-			playerRedAvatar = currentSelected
-			print(playerRedAvatar)
+			playerBlackAvatar = currentSelected
+			print(playerBlackAvatar)
 			nextSelector.previousSelected = true
 			mainScript._red = currentSelected
 			
-			if avatarBlack.visible == true:
+			if avatarBlue.visible == true:
 				nextSelector.visible = true
