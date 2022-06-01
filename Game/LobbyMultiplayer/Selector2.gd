86b21c4e2d8029c8a9ccc300e5375f084e268b49
Ewelina Walkusz <ewelina.walkusz@protonmail.com>
2022-06-01 22:53:53 +0200

    add Selector2

--- Game/LobbyMultiplayer/Selector2.gd
+++ Game/LobbyMultiplayer/Selector2.gd
@@ -1,7 +1,7 @@
 extends Sprite
 
 
-var avatars_1 = []
+var avatars_2 = []
 var currentSelected = 0		# Spot of the FrameSelector within the avatars_1[]
 var currentColumnSpot = 0	# Spot of the FrameSelector based on the column
 var currentRowSpot = 0
@@ -11,19 +11,19 @@ var previousSelected = false
 export (Texture) var selectorTexture
 export (int) var amountOfRows = 3      # The total amount of rows the character select is able to show 
 export (Vector2) var avatarsOffset
-export (int) var playerBlackAvatar
+export (int) var playerBlueAvatar
 
 onready var mainScript = get_parent()
 onready var gridContainer = get_node("../GridContainer")
-onready var selector1 = get_node("../Selector1")
-onready var selectorLabel1 = selector1.get_node("Label")
-onready var nextSelector = get_node("../Selector2")
-onready var avatarBlue = get_node("/root/LobbyMulti/GridContainer/GridContainerAvatars/HBoxContainerAvatar1/Avatar1Blue")
+onready var selector2 = get_node("../Selector2")
+onready var selectorLabel2 = selector2.get_node("Label")
+onready var nextSelector = get_node("../Selector3")
+onready var avatarYellow = get_node("/root/LobbyMulti/GridContainer/GridContainerAvatars/HBoxContainerAvatar1/Avatar1Yellow")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for nameOfAvatars_1 in get_tree().get_nodes_in_group("Avatars_1"):
-		avatars_1.append(nameOfAvatars_1)
+	for nameOfAvatars_2 in get_tree().get_nodes_in_group("Avatars_2"):
+		avatars_2.append(nameOfAvatars_2)
 	
 	texture = selectorTexture
 
@@ -49,10 +49,10 @@ func _input(event):
 
 		if event.scancode == KEY_ENTER:
 			enterPressed = true
-			playerBlackAvatar = currentSelected
-			print(playerBlackAvatar)
+			playerBlueAvatar = currentSelected
+			print(playerBlueAvatar)
 			nextSelector.previousSelected = true
-			mainScript._black = currentSelected
+			mainScript._blue = currentSelected
 			
-			if avatarBlue.visible == true:
+			if avatarYellow.visible == true:
 				nextSelector.visible = true
